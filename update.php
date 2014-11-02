f28989a9fb39648f3b2bb63727786622a29e611b
Hai Lu <hai.lu@c-mg.com>
2014-11-02 12:57:54 +0700

    Force update

--- update.php
+++ update.php
@@ -25,28 +25,33 @@ if (strcasecmp($gEvent, 'push') == 0) {
     echo 'Detect push event.';
     $data = json_decode(file_get_contents('php://input'),true);
     if (isset($data['ref']) && strcasecmp($data['ref'], 'refs/heads/master') == 0) {
-        $logs = "";
-        $command =
-            'id 2>&1;'.
-            'export COMPOSER_HOME="'.getenv("P_BASE_DIR").'";'.
-            'php composer.phar install 2>&1;'.
-            'export AWS_ACCESS_KEY_ID="'.getenv("AWS_ACCESS_KEY_ID").'" 2>&1;'.
-            'export AWS_SECRET_ACCESS_KEY="'.getenv("AWS_SECRET_ACCESS_KEY").'" 2>&1;'.
-            'export LANG="en_GB.UTF-8" 2>&1;'.
-            'export AWS_REGION="'.getenv("AWS_REGION").'" 2>$1'.
-            'export LANGUAGE=en_GB:en 2>&1;'.
-            'cd '.getenv("P_BASE_DIR").';'.
-            'git reset HEAD --hard;'.
-            'git pull origin master 2>&1;'.
-            'rake deploy 2>&1;'.
-            'git aws.push 2>&1';
-        $handle = popen($command, 'r');
-        while ($line = fread($handle, 100)){
-            echo $line;
-            $logs .= $line;
-        }
-        pclose($handle);
 
+        $logs = "";
+        try {
+            $command =
+                'id 2>&1;' .
+                'export COMPOSER_HOME="' . getenv("P_BASE_DIR") . '";' .
+                'php composer.phar install 2>&1;' .
+                'export AWS_ACCESS_KEY_ID="' . getenv("AWS_ACCESS_KEY_ID") . '" 2>&1;' .
+                'export AWS_SECRET_ACCESS_KEY="' . getenv("AWS_SECRET_ACCESS_KEY") . '" 2>&1;' .
+                'export LANG="en_GB.UTF-8" 2>&1;' .
+                'export AWS_REGION="' . getenv("AWS_REGION") . '" 2>$1' .
+                'export LANGUAGE=en_GB:en 2>&1;' .
+                'cd ' . getenv("P_BASE_DIR") . ';' .
+                'git reset HEAD --hard;' .
+                'git pull origin master 2>&1;' .
+                'rake deploy 2>&1;' .
+                'git aws.push 2>&1';
+            $handle = popen($command, 'r');
+            while ($line = fread($handle, 100)) {
+                echo $line;
+                $logs .= $line;
+            }
+            pclose($handle);
+        } catch(Exception $e) {
+            echo $e->getMessage();
+            $logs .= 'error: '.$e->getMessage();
+        }
         $bucket = 'logs.trollvd.com';
         $dt = new DateTime();
         $keyname =$dt->format('Y-m-d').'/'.$dt->format('H:i:s').'.log';
